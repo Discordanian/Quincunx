62356a1fbe5dfe554bbc53cb88281bf55c499593
Kurt Schwind <kurt.schwind@gmail.com>
2026-02-14 08:54:39 -0600

    Sound

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,10 +1,26 @@
 extends RigidBody2D
 
-const PingSound = preload("res://Assets/Sound/A.ogg")
+const PING_SOUNDS = [
+	preload("res://Assets/Sound/A.ogg"),
+	preload("res://Assets/Sound/D.ogg"),
+	preload("res://Assets/Sound/F.ogg")
+]
 
-func _on_body_entered(_body: Node) -> void:
-    print("Body Entered")
+var _audio_player: AudioStreamPlayer
 
+func _ready() -> void:
+	contact_monitor = true
+	max_contacts_reported = 16
+	_audio_player = AudioStreamPlayer.new()
+	add_child(_audio_player)
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("pin"):
+		_play_random_ping()
 
 func _on_body_shape_entered(_body_rid: RID, _body: Node, _body_shape_index: int, _local_shape_index: int) -> void:
-    print("on_body_shape_entered")
+	pass
+
+func _play_random_ping() -> void:
+	_audio_player.stream = PING_SOUNDS.pick_random()
+	_audio_player.play()
--- scripts/splash.gd
+++ scripts/splash.gd
@@ -3,9 +3,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -24.0)
 
 
 func _on_button_button_down() -> void:
-    print("Button Down")
-    get_tree().change_scene_to_file("res://scenes/board.tscn")
+	print("Button Down")
+	get_tree().change_scene_to_file("res://scenes/board.tscn")
