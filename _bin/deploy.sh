5a354adf6af6b730a3dba89aaa5605f609eba770
Kurt Schwind <kurt.schwind@gmail.com>
2026-02-15 07:42:36 -0600

    .

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -10,5 +10,9 @@ then
   exit 1
 fi
 
+# rm "$BUILD_DIR/*"
+
+# /Applications/Godot v4.6.app/Contents/MacOS/Godot --headless --export-release Quincunx ./build/index.html
+
 
 rsync -avz --progress --delete "$BUILD_DIR/" "$REMOTE_PATH"
